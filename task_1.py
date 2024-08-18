ea7c2256dc12dec522d2b44b0137e3a934fd813d
yar kolin <x-t-p@ya.ru>
2024-08-18 18:42:53 +0500

    not_final_2.0

--- task_1.py
+++ task_1.py
@@ -10,7 +10,7 @@ class Student:
         if isinstance(lector, Lecturer) and course in self.finished_courses and course in lector.courses_attached:
             rating_scale = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
             if course in lector.grades and grade in rating_scale:
-                lector.grades[course] += [grade]
+                course.grades[course].append(grade)
             else:
                 lector.grades[course] = [grade]
         else:
@@ -91,9 +91,7 @@ class Lecturer(Mentor):
 
 
 class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-    def rate_hw(self, student, course, grade):
+     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
@@ -101,90 +99,90 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-    def __str__(self):
+     def __str__(self):
         return f'Имя: {self.name} \nФамилия: {self.surname}'
 
     # студент 1
-    student_1 = Student('Джессика', 'Альба', 'ж')
-    student_1.finished_courses = ['Git']
-    student_1.courses_in_progress = ['CSS', 'Java']
-    print(student_1)
+student_1 = Student('Джессика', 'Альба', 'ж')
+student_1.finished_courses = ['Git']
+student_1.courses_in_progress = ['CSS', 'Java']
+print(student_1)
 
-    # студент 2
-    student_2 = Student('Бэтмен', 'Иванов', 'м')
-    student_2.finished_courses = ['C#']
-    student_2.courses_in_progress = ['SQL', 'Java']
-    print(student_2)
+# студент 2
+student_2 = Student('Бэтмен', 'Иванов', 'м')
+student_2.finished_courses = ['C#']
+student_2.courses_in_progress = ['SQL', 'Java']
+print(student_2)
 
-    # лектор 1
-    lecturer_1 = Lecturer('Альфред', 'Петров')
-    lecturer_1.courses_attached = ['SQL']
-    print(lecturer_1)
+# лектор 1
+lecturer_1 = Lecturer('Альфред', 'Петров')
+lecturer_1.courses_attached = ['SQL']
+print(lecturer_1)
 
-    # лектор 2
-    lecturer_2 = Lecturer('Борис', 'Кац')
-    lecturer_2.courses_attached = ['Java']
-    print(lecturer_2)
+# лектор 2
+lecturer_2 = Lecturer('Борис', 'Кац')
+lecturer_2.courses_attached = ['Java']
+print(lecturer_2)
 
-    # ревивер 1
-    reviewer_1 = Reviewer('Джон', 'Сидоров')
-    reviewer_1.courses_attached = ['Git']
-    print(reviewer_1)
+# ревивер 1
+reviewer_1 = Reviewer ('Джон', 'Сидоров')
+reviewer_1.courses_attached = ['Git']
+print(reviewer_1)
 
-    # ревивер 2
-    reviewer_2 = Reviewer('Ким', 'Чен Ыр')
-    reviewer_2.courses_attached = ['С++']
-    print(reviewer_2)
+# ревивер 2
+reviewer_2 = Reviewer('Ким', 'Чен Ыр')
+reviewer_2.courses_attached = ['С++']
+print(reviewer_2)
 
-    # проверка методов
-    student_1.rate_lec(lecturer_1, 'Git', 10)  # несотв. лектора и курса
-    student_1.rate_lec(lecturer_1, 'SQL', 8)
-    student_2.rate_lec(lecturer_1, 'SQL', 10)
-    student_2.rate_lec(lecturer_2, 'Java', 5)
-    print('Оценки 1-го преподавателя', lecturer_1.grades)
+# проверка методов
+student_1.rate_lec(lecturer_1, 'Java', 10)  # несотв. лектора и курса
+student_1.rate_lec(lecturer_1, 'SQL', 8)
+student_2.rate_lec(lecturer_1, 'SQL', 10)
+student_2.rate_lec(lecturer_2, 'SQL', 5)
+print('Оценки 1-го преподавателя', lecturer_1.grades)
 
-    reviewer_1.rate_hw(student_1, 'Git', 7)
-    reviewer_1.rate_hw(student_1, 'C#', 7)
-    print(f'Оценки 1-го студента {student_1.grades}')
+reviewer_1.rate_hw(student_1, 'CSS', 7)
+reviewer_1.rate_hw(student_1, 'Java', 7)
+print(f'Оценки 1-го студента {student_1.grades}')
 
-    reviewer_2.rate_hw(student_2, 'Git', 9)
-    reviewer_1.rate_hw(student_2, 'C#', 6)
-    print(f'Оценки 2-го студента {student_2.grades}')
+reviewer_2.rate_hw(student_2, 'Git', 9)
+reviewer_1.rate_hw(student_2, 'C#', 6)
+print(f'Оценки 2-го студента {student_2.grades}')
 
-    # сравнение студентов
-    print(student_1.av_grades(), student_2.av_grades())
-    print(student_1 > student_2)
-    print(student_1 == student_2)
-    print(student_1 < student_2)
+# сравнение студентов
+print(student_1.av_grades(), student_2.av_grades())
+print(student_1 > student_2)
+print(student_1 == student_2)
+print(student_1 < student_2)
 
-    # сравнение лекторов
-    print(lecturer_1.av_grades(), lecturer_2.av_grades())
-    print(lecturer_1 > lecturer_2)
-    print(lecturer_1 == lecturer_2)
-    print(lecturer_1 < lecturer_2)
+# сравнение лекторов
+print(lecturer_1.av_grades(), lecturer_2.av_grades())
+print(lecturer_1 > lecturer_2)
+print(lecturer_1 == lecturer_2)
+print(lecturer_1 < lecturer_2)
 
-    # методы подсчета средней оценки
+# средняя оценка
 
-    def av_rating_st(students: list, course: str):
-        rating, count = 0, 0
-        for student in students:
-            if student.grades.get(course, None):
-                rating += sum(student.grades[course])
-                count += 1
-        return rating / count if count != 0 else 'нет данных'
+def av_rating_st(students: list, course: str):
+    rating, count = 0, 0
+    for student in students:
+        if student.grades.get(course, None):
+            rating += sum(student.grades[course])
+            count += 1
+    return rating / count if count != 0 else 'нет данных'
 
-    def av_rating_lec(lectors: list, course: str):
-        rating, count = 0, 0
-        for lector in lectors:
-            if lector.grades.get(course, None):
-                rating += sum(lector.grades[course])
-                count += 1
-        return rating / count if count != 0 else 'нет данных'
+def av_rating_lec(lectors: list, course: str):
+    rating, count = 0, 0
+    for lector in lectors:
+        if lector.grades.get(course, None):
+            rating += sum(lector.grades[course])
+            count += 1
+    return rating / count if count != 0 else 'нет данных'
 
-    # print(av_rating_st([student_1, student_2], 'Python'))
-    # print(av_rating_st([student_1, student_2], 'Git'))
-    # print(av_rating_st([student_1, student_2], 'Java'))
-    # print()
-    # print(av_rating_lec([lecturer_1, lecturer_2], 'Python'))
-    # print(av_rating_lec([lecturer_1, lecturer_2], 'Git'))
-    # print(av_rating_lec([lecturer_1, lecturer_2], 'Java'))
+print(av_rating_st([student_1, student_2], 'Python'))
+print(av_rating_st([student_1, student_2], 'Git'))
+print(av_rating_st([student_1, student_2], 'Java'))
+print()
+print(av_rating_lec([lecturer_1, lecturer_2], 'Python'))
+print(av_rating_lec([lecturer_1, lecturer_2], 'Git'))
+print(av_rating_lec([lecturer_1, lecturer_2], 'Java'))
